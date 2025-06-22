271e385e8e7b5c2758c060cc08a65dc70cd146ed
bekajunior254 <chanagawaelisha308@gmail.com>
2025-06-22 19:20:12 +0300

    Updating the localhost in the register page

--- frontend/school/src/pages/Register.jsx
+++ frontend/school/src/pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5241/api/auth/register", form);
+      await axios.post("http://localhost:5000/api/auth/register", form);
       alert("Registration successful!");
     } catch (error) {
       alert("Registration failed.");

06ad4bb73850a45076f3dea1bcee9e3f96fd9779
bekajunior254 <chanagawaelisha308@gmail.com>
2025-06-22 20:18:43 +0300

    Changing my database  , deleting the existing migrations and creating new migrations

--- backend/BackendApi/Migrations/20250622162357_InitialCreate.Designer.cs
+++ backend/BackendApi/Migrations/20250622162357_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackendApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250620134225_UpdateAppUserModel")]
-    partial class UpdateAppUserModel
+    [Migration("20250622162357_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)

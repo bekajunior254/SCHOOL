1fccf2c0d68d9c7dba08022760e53d8a6529a136
bekajunior254 <chanagawaelisha308@gmail.com>
2025-06-23 12:42:24 +0300

    Inputing the correct port for the login page

--- frontend/school/src/pages/Login.jsx
+++ frontend/school/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     setError(""); 
 
     try {
-      const res = await axios.post("https://localhost:5001/api/auth/login", {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
         username,
         password,
       });

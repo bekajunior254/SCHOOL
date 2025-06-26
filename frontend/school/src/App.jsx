61d2e3143463e02b4d110e62a5c2deca6b8b9615
bekajunior254 <chanagawaelisha308@gmail.com>
2025-06-26 16:39:39 +0300

    Updating  the App.jsx

--- frontend/school/src/App.jsx
+++ frontend/school/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
+import AssignRolePage from "./pages/AssignRolePage.jsx";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
        <Route path="/admin" element={<AdminDashboard />} />
-     <Route path="/admin/assign-role" element={<AssignRolePage />} />
+      <Route path="/admin/assign-role" element={<AssignRolePage />} />
 
       </Routes>
     </Router>
